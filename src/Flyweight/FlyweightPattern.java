95f5375278144f4b4c7d6ad5e6e209e609b5afa5
RX1226 <0979097955s@gmail.com>
2021-06-25 10:57:41 +0800

    add Comment

--- src/Flyweight/FlyweightPattern.java
+++ src/Flyweight/FlyweightPattern.java
@@ -3,6 +3,12 @@ package Flyweight;
 import Flyweight.Pattern.PlantManager;
 import Flyweight.Pattern.StudentManager;
 
+/**
+ 享元模式/蠅量級模式 Flyweight Pattern
+ 把需要量產的物件獨立出來只保留功能
+ 再用一個管理物件來記錄原本要產生物件的狀態
+ 在數量越多的時候越可以省下更多的空間
+ */
 public class FlyweightPattern {
     public static void main(String[] args) {
         //單純享元模式
